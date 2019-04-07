e461c1e500c26c0d3fdd5cd5b7e2a9fe50a744e0
Tyler M <tyler@bitcraft-fl.com>
2019-04-07 11:58:01 -0400

    Initial code.

---
+++ OrangePeel/DependencyInjectedAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TylerDM.OrangePeel
+{
+	public enum ServiceLifetime
+	{
+		Singleton,
+		Scoped,
+		Transient
+	}
+
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+	public class DependencyInjectedAttribute : Attribute
+	{
+		public ServiceLifetime ServiceLifetime { get; }
+		public IReadOnlyCollection<Type> InterfaceTypes { get; }
+
+		public DependencyInjectedAttribute(ServiceLifetime serviceLifetime)
+		{
+			ServiceLifetime = serviceLifetime;
+		}
+
+		public DependencyInjectedAttribute(ServiceLifetime serviceLifetime, params Type[] interfaceTypes)
+		{
+			ServiceLifetime = serviceLifetime;
+			InterfaceTypes = interfaceTypes ?? throw new ArgumentNullException(nameof(interfaceTypes));
+		}
+	}
+}
---
+++ OrangePeel/IServiceCollectionExt.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace TylerDM.OrangePeel
+{
+	public static class IServiceCollectionExt
+	{
+		public static void AddOrangePeeledServices(this IServiceCollection services)
+		{
+			if (services == null) throw new ArgumentNullException(nameof(services));
+
+			foreach (var type in getAllTypesInAssembly())
+			{
+				var attributes = type.GetCustomAttributes<DependencyInjectedAttribute>();
+				if (!attributes.Any()) continue;
+
+				foreach (var attribute in attributes)
+				{
+					switch (attribute.ServiceLifetime)
+					{
+						case ServiceLifetime.Singleton:
+							services.AddSingleton(type);
+							foreach(var interfaceType in attribute.InterfaceTypes)
+								services.AddSingleton(interfaceType, type);
+							break;
+						case ServiceLifetime.Scoped:
+							services.AddScoped(type);
+							foreach (var interfaceType in attribute.InterfaceTypes)
+								services.AddScoped(interfaceType, type);
+							break;
+						case ServiceLifetime.Transient:
+							services.AddTransient(type);
+							foreach (var interfaceType in attribute.InterfaceTypes)
+								services.AddTransient(interfaceType, type);
+							break;
+					}
+				}
+			}
+		}
+
+		private static IEnumerable<Type> getAllTypesInAssembly()
+		{
+			var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+			foreach (var assembly in assemblies)
+			{
+				var types = assembly.GetTypes();
+				foreach (var type in types)
+					yield return type;
+			}
+		}
+	}
+}
