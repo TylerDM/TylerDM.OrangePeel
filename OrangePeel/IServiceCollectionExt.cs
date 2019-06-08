2cbba01cd38c628f31710d1136d3c327318d27a1
Tyler M <tyler@bitcraft-fl.com>
2019-06-08 15:10:27 -0400

    Register single instance for every type it represents.

--- OrangePeel/IServiceCollectionExt.cs
+++ OrangePeel/IServiceCollectionExt.cs
@@ -20,41 +20,34 @@ namespace TylerDM.OrangePeel
         foreach (var attribute in attributes)
         {
           services.add(attribute.ServiceLifetime, type);
-          foreach (var interfaceType in attribute.InterfaceTypes)
-            services.add(attribute.ServiceLifetime, interfaceType, type);
+          services.add(attribute.ServiceLifetime, type, attribute.InterfaceTypes);
         }
       }
     }
 
-    private static void add(this IServiceCollection services, ServiceLifetime serviceLifetime, Type type)
+    private static void add(this IServiceCollection services, ServiceLifetime serviceLifetime, Type concreteType, IEnumerable<Type> interfaceTypes)
     {
-      switch (serviceLifetime)
-      {
-        case ServiceLifetime.Singleton:
-          services.AddSingleton(type);
-          break;
-        case ServiceLifetime.Scoped:
-          services.AddScoped(type);
-          break;
-        case ServiceLifetime.Transient:
-          services.AddTransient(type);
-          break;
-      }
+      foreach (var interfaceType in interfaceTypes)
+        services.add(serviceLifetime, concreteType, interfaceType);
     }
 
-    private static void add(this IServiceCollection services, ServiceLifetime serviceLifetime, Type interfaceType, Type concreteType)
+    private static void add(this IServiceCollection services, ServiceLifetime serviceLifetime, Type concreteType, Type interfaceType = null)
     {
+      interfaceType = interfaceType ?? concreteType;
+
       switch (serviceLifetime)
       {
         case ServiceLifetime.Singleton:
-          services.AddSingleton(interfaceType, concreteType);
+          services.AddSingleton(interfaceType, x => x.GetService(concreteType));
           break;
         case ServiceLifetime.Scoped:
-          services.AddScoped(interfaceType, concreteType);
+          services.AddScoped(interfaceType, x => x.GetService(concreteType));
           break;
         case ServiceLifetime.Transient:
-          services.AddTransient(interfaceType, concreteType);
+          services.AddTransient(interfaceType, x => x.GetService(concreteType));
           break;
+        default:
+          throw new ArgumentOutOfRangeException(nameof(serviceLifetime));
       }
     }
 
