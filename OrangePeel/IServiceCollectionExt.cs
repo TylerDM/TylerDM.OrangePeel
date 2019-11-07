44c825a4e3d17fa3e35724000eb5e2fc6f379534
Tyler Mays <16845057+FireflyX@users.noreply.github.com>
2019-11-07 14:48:43 -0500

    Prevent double registration of assemblies.

--- OrangePeel/IServiceCollectionExt.cs
+++ OrangePeel/IServiceCollectionExt.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -8,16 +9,31 @@ namespace TylerDM.OrangePeel
 {
 	public static class IServiceCollectionExt
 	{
+		private static readonly ConcurrentBag<string> _orangePeeledAssemblies = new ConcurrentBag<string>();
+
 		public static AddServicesResult AddOrangePeeledServices(this IServiceCollection services)
 		{
 			if (services == null) throw new ArgumentNullException(nameof(services));
 
+			//This must execute here and CANNOT be moved into a different method as then the calling assembly would be Orange Peel itself.
+			var callingAssembly = Assembly.GetCallingAssembly();
+
+			//Make sure we orangle peel any given assembly only once.
+			var callingAssemblyName = callingAssembly.FullName ?? throw new Exception("Assembly name not found.");
+			lock (_orangePeeledAssemblies)
+			{
+				if (_orangePeeledAssemblies.Contains(callingAssemblyName)) return new AddServicesResult(0, 0);
+				_orangePeeledAssemblies.Add(callingAssemblyName);
+			}
+
+			return services.addAllTypesFromAssembly(callingAssembly);
+		}
+
+		private static AddServicesResult addAllTypesFromAssembly(this IServiceCollection services, Assembly assembly)
+		{
 			var addedServices = 0;
 			var addedInterfaces = 0;
-
-			//This must execute here and CANNOT be moved into getTypesFromCallingAssembly() as then the calling assembly would be itself.
-			var typesFromCallingAssembly = Assembly.GetCallingAssembly().GetTypes();
-			foreach (var type in typesFromCallingAssembly)
+			foreach (var type in assembly.GetTypes())
 			{
 				var attributes = type.GetCustomAttributes<DependencyInjectableAttribute>();
 				if (!attributes.Any()) continue;
@@ -27,31 +43,31 @@ namespace TylerDM.OrangePeel
 
 				if (type.IsAbstract) throw new Exception($"Cannot register abstract class \"{type.FullName}\".");
 
-				services.Add(serviceLifetime, type);
+				services.add(serviceLifetime, type);
 				addedServices++;
 
 				if (attribute.InterfaceTypes.Any())
 				{
 					var interfaceTypes = attribute.InterfaceTypes;
 					addedInterfaces += interfaceTypes.Count;
-					services.Add(serviceLifetime, type, interfaceTypes);
+					services.add(serviceLifetime, type, interfaceTypes);
 				}
 			}
 
 			return new AddServicesResult(addedServices, addedInterfaces);
 		}
 
-		public static void Add(this IServiceCollection services, ServiceLifetime serviceLifetime, Type service, IEnumerable<Type> interfaceTypes)
+		private static void add(this IServiceCollection services, ServiceLifetime serviceLifetime, Type service, IEnumerable<Type> interfaceTypes)
 		{
 			if (services == null) throw new ArgumentNullException(nameof(services));
 			if (service == null) throw new ArgumentNullException(nameof(service));
 			if (interfaceTypes == null) throw new ArgumentNullException(nameof(interfaceTypes));
 
 			foreach (var interfaceType in interfaceTypes)
-				services.Add(serviceLifetime, service, interfaceType);
+				services.add(serviceLifetime, service, interfaceType);
 		}
 
-		public static void Add(this IServiceCollection services, ServiceLifetime serviceLifetime, Type service)
+		private static void add(this IServiceCollection services, ServiceLifetime serviceLifetime, Type service)
 		{
 			if (services == null) throw new ArgumentNullException(nameof(services));
 			if (service == null) throw new ArgumentNullException(nameof(service));
@@ -73,7 +89,7 @@ namespace TylerDM.OrangePeel
 			}
 		}
 
-		public static void Add(this IServiceCollection services, ServiceLifetime serviceLifetime, Type service, Type? interfaceType)
+		private static void add(this IServiceCollection services, ServiceLifetime serviceLifetime, Type service, Type? interfaceType)
 		{
 			if (services == null) throw new ArgumentNullException(nameof(services));
 			interfaceType ??= service ?? throw new ArgumentNullException(nameof(service));
