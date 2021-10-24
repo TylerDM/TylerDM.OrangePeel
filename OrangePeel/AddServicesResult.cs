2fe00e1861d26234d6a917cd52a2bf1e24992f0b
Tyler M <16845057+TylerDM@users.noreply.github.com>
2021-10-23 20:51:33 -0400

    Clean.

--- OrangePeel/AddServicesResult.cs
+++ OrangePeel/AddServicesResult.cs
@@ -1,16 +1,4 @@
 ﻿namespace TylerDM.OrangePeel
 {
-	public class AddServicesResult
-	{
-		public int AddedServices { get; }
-		public int AddedInterfaces { get; }
-
-		public static AddServicesResult Empty { get; } = new AddServicesResult(0, 0);
-
-		public AddServicesResult(int addedServices, int addedInterfaces)
-		{
-			AddedServices = addedServices;
-			AddedInterfaces = addedInterfaces;
-		}
-	}
+	public record AddServicesResult(int AddedServices, int AddedInterfaces);
 }
