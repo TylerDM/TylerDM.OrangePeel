072847a0d46e39da491ee8df41da904a71638e46
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-08-04 18:30:04 -0400

    Rewrite.

--- TylerDM.OrangePeel/DependencyInjectableAttribute.cs
+++ TylerDM.OrangePeel/DependencyInjectableAttribute.cs
@@ -4,17 +4,18 @@
 public sealed class DependencyInjectableAttribute : Attribute
 {
 	public ServiceLifetime ServiceLifetime { get; }
-	public IReadOnlyCollection<Type> InterfaceTypes { get; }
+	public IReadOnlyCollection<Type> ServiceTypes { get; }
 
-	public DependencyInjectableAttribute(ServiceLifetime serviceLifetime)
+	public DependencyInjectableAttribute(ServiceLifetime serviceLifetime) : this(serviceLifetime, [])
 	{
-		ServiceLifetime = serviceLifetime;
-		InterfaceTypes = Array.Empty<Type>();
 	}
 
 	public DependencyInjectableAttribute(ServiceLifetime serviceLifetime, params Type[] interfaceTypes)
 	{
+		if (Enum.IsDefined(serviceLifetime) == false)
+			throw new ArgumentOutOfRangeException(nameof(serviceLifetime));
+
+		ServiceTypes = interfaceTypes ?? throw new ArgumentNullException(nameof(interfaceTypes));
 		ServiceLifetime = serviceLifetime;
-		InterfaceTypes = interfaceTypes ?? throw new ArgumentNullException(nameof(interfaceTypes));
 	}
 }
--- TylerDM.OrangePeel/IServiceCollectionExt.cs
+++ TylerDM.OrangePeel/IServiceCollectionExt.cs
@@ -3,122 +3,67 @@
 public static class IServiceCollectionExt
 {
 	#region fields
-	private static readonly ConcurrentBag<string> _orangePeeledAssemblies = new();
+	private static readonly List<Assembly> _orangePeeledAssemblies = [];
 	#endregion
 
 	#region methods
-	public static AddServicesResult AddOrangePeeledServices(this IServiceCollection services)
+	public static IReadOnlyCollection<ServiceDescriptor> AddOrangePeeledServices(this IServiceCollection services)
 	{
+		ArgumentNullException.ThrowIfNull(services);
+
 		//This must execute here and CANNOT be moved into a different method as then the calling assembly would be Orange Peel itself.
 		var assembly = Assembly.GetCallingAssembly();
 
 		//Make sure we orange peel any given assembly only once.
-		var callingAssemblyName = assembly.FullName ?? throw new Exception("Assembly name not found.");
 		lock (_orangePeeledAssemblies)
 		{
-			if (_orangePeeledAssemblies.Contains(callingAssemblyName)) return new(0, 0);
-			_orangePeeledAssemblies.Add(callingAssemblyName);
+			if (_orangePeeledAssemblies.Contains(assembly)) return [];
+			_orangePeeledAssemblies.Add(assembly);
 		}
 
-		return services.addAllTypesFromAssembly(assembly);
+		return services.addAll(assembly);
 	}
 	#endregion
 
 	#region private methods
-	private static AddServicesResult addAllTypesFromAssembly(this IServiceCollection services, Assembly assembly)
+	private static List<ServiceDescriptor> addAll(this IServiceCollection services, Assembly assembly)
 	{
-		var addedServices = 0;
-		var addedInterfaces = 0;
-		foreach (var type in getTypesSafely(assembly))
+		var list = new List<ServiceDescriptor>();
+		foreach (var (serviceType, attribute) in getServiceTypes(assembly))
 		{
-			var attributes = type.GetCustomAttributes<DependencyInjectableAttribute>();
-			if (!attributes.Any()) continue;
+			if (serviceType.IsAbstract)
+				throw new Exception($"Cannot register abstract class {serviceType.FullName}.");
 
-			if (type.IsAbstract) throw new Exception($"Cannot register abstract class \"{type.FullName}\".");
-
-			var attribute = attributes.First();
-			var serviceLifetime = attribute.ServiceLifetime;
-
-			services.add(serviceLifetime, type);
-			addedServices++;
-
-			if (attribute.InterfaceTypes.Any())
-			{
-				var interfaceTypes = attribute.InterfaceTypes;
-				addedInterfaces += interfaceTypes.Count;
-				services.add(serviceLifetime, type, interfaceTypes);
-			}
+			list.AddRange(services.add(serviceType, attribute));
 		}
-
-		return new AddServicesResult(addedServices, addedInterfaces);
+		return list;
 	}
 
-	private static IEnumerable<Type> getTypesSafely(Assembly assembly)
+	private static List<ServiceDescriptor> add(this IServiceCollection services, Type implementationType, DependencyInjectableAttribute attribute)
 	{
-		//Less dependencies are loaded in Load() and LoadFrom().
-		assembly = Assembly.Load(assembly.GetName());
-		try
-		{
-			return assembly.GetTypes();
-		}
-		catch (ReflectionTypeLoadException exception)
-		{
-			return exception.Types.OfType<Type>();
-		}
+		var list = new List<ServiceDescriptor>(1 + attribute.ServiceTypes.Count);
+		var lifetime = attribute.ServiceLifetime;
+
+		list.Add(services.add(lifetime, implementationType));
+		foreach (var serviceType in attribute.ServiceTypes)
+			list.Add(services.add(lifetime, implementationType, serviceType));
+
+		return list;
 	}
 
-	private static void add(this IServiceCollection services, ServiceLifetime serviceLifetime, Type service, IEnumerable<Type> interfaceTypes)
+	private static IEnumerable<(Type ServiceType, DependencyInjectableAttribute Attribute)> getServiceTypes(Assembly assembly) =>
+		from type in assembly.GetTypes()
+		let attribute = type.GetCustomAttributes<DependencyInjectableAttribute>().FirstOrDefault()
+		where attribute is not null
+		select (type, attribute);
+
+	private static ServiceDescriptor add(this IServiceCollection services, ServiceLifetime lifetime, Type implementationType, Type? serviceType = null)
 	{
-		if (services is null) throw new ArgumentNullException(nameof(services));
-		if (service is null) throw new ArgumentNullException(nameof(service));
-		if (interfaceTypes is null) throw new ArgumentNullException(nameof(interfaceTypes));
+		serviceType ??= implementationType;
 
-		foreach (var interfaceType in interfaceTypes)
-			services.add(serviceLifetime, service, interfaceType);
-	}
-
-	private static void add(this IServiceCollection services, ServiceLifetime serviceLifetime, Type service)
-	{
-		if (services is null) throw new ArgumentNullException(nameof(services));
-		if (service is null) throw new ArgumentNullException(nameof(service));
-
-		switch (serviceLifetime)
-		{
-			case ServiceLifetime.Singleton:
-				services.AddSingleton(service);
-				break;
-			case ServiceLifetime.Scoped:
-				services.AddScoped(service);
-				break;
-			case ServiceLifetime.Transient:
-				services.AddTransient(service);
-				break;
-
-			default:
-				throw new ArgumentOutOfRangeException(nameof(serviceLifetime));
-		}
-	}
-
-	private static void add(this IServiceCollection services, ServiceLifetime serviceLifetime, Type service, Type? interfaceType)
-	{
-		if (services is null) throw new ArgumentNullException(nameof(services));
-		interfaceType ??= service ?? throw new ArgumentNullException(nameof(service));
-
-		switch (serviceLifetime)
-		{
-			case ServiceLifetime.Singleton:
-				services.AddSingleton(interfaceType, x => x.GetRequiredService(service));
-				break;
-			case ServiceLifetime.Scoped:
-				services.AddScoped(interfaceType, service);
-				break;
-			case ServiceLifetime.Transient:
-				services.AddTransient(interfaceType, service);
-				break;
-
-			default:
-				throw new ArgumentOutOfRangeException(nameof(serviceLifetime));
-		}
+		var descriptor = new ServiceDescriptor(serviceType, implementationType, lifetime);
+		services.Add(descriptor);
+		return descriptor;
 	}
 	#endregion
 }
