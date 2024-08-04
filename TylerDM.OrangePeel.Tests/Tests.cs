6ad68d6f8370d43003cbbd8c6e50eec73116d038
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-08-04 19:19:13 -0400

    Add materialization test to be safe.

--- TylerDM.OrangePeel.Tests/Tests.cs
+++ TylerDM.OrangePeel.Tests/Tests.cs
@@ -1,13 +1,17 @@
-﻿namespace TylerDM.OrangePeel.Tests;
+﻿using System.Runtime.InteropServices;
+
+namespace TylerDM.OrangePeel.Tests;
 
 public static class Tests
 {
 	private static readonly ServiceCollection _serviceCollection;
+	private static readonly ServiceProvider _serviceProvider;
 
 	static Tests()
 	{
 		_serviceCollection = new ServiceCollection();
 		_serviceCollection.AddOrangePeeledServices();
+		_serviceProvider = _serviceCollection.BuildServiceProvider();
 	}
 
 	[Test]
@@ -28,5 +32,15 @@ public static class Tests
 		var descriptorA = _serviceCollection.First(x => x.ServiceType == typeof(IInterfaceA));
 		if (descriptorA.ImplementationType != typeof(ServiceA))
 			throw new Exception($"{nameof(ServiceA)} was not registered as {nameof(IInterfaceA)}");
+	}
+
+	[Test]
+	public static void Materialization()
+	{
+		var interfaceA = _serviceProvider.GetRequiredService<IInterfaceA>();
+		if (interfaceA is not ServiceA)
+			throw new Exception($"{nameof(ServiceProvider)} did not return {nameof(ServiceA)} for {nameof(IInterfaceA)}.");
+
+		var serviceB = _serviceProvider.GetRequiredService<ServiceB>();
 	}
 }
